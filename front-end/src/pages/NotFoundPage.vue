5e2feb386221f81630ea8382bcb1d23bbd76141e
Chamidu Chamodya <47558333+ChamiduChamodya@users.noreply.github.com>
2023-05-04 06:52:42 +0000

    feat(UI): Ui with temp data

---
+++ front-end/src/pages/NotFoundPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>404: Pahe Not Found</h1>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage"
+}
+</script>
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList :cartItems="cartItems" />
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    Your Cart is Empty
+  </div>
+</template>
+
+<script>
+import { cartItems } from '@/temp-data';
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+
+export default {
+  name: "ShoppingCartPage",
+  data() {
+    return {
+      cartItems
+    }
+  },
+  components: {
+    ShoppingCartList
+  }
+}
+</script>
